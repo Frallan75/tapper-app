b230b3bbfe74044e3046b128692d024bf5f84711
Francisco Claret <franciscoclaret@Franciscos-MacBook-Pro.local>
2016-01-17 09:01:09 -0800

    added the projekt

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -10,15 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var tapLabel: UILabel!
+    
+    var numberOfTapsToWin = 0
+    var tappedTimes = 0
+    
+    @IBAction func playButtonPressed(sender: UIButton!) {
+        
+        guard let numberOfTapsToWinText = textField.text where textField.text != "" && Int(textField.text!) != nil else {
+            alert()
+            return}
+        print(numberOfTapsToWinText)
+        
+        numberOfTapsToWin = Int(numberOfTapsToWinText)!
+        
+        logo.hidden = true
+        playButton.hidden = true
+        textField.hidden = true
+        tapButton.hidden = false
+        tapLabel.hidden = false
+        
+        tapLabel.text = "Number of taps to win = \(numberOfTapsToWin)"
+    
     }
+    
+    @IBAction func tapButtonTapped(sender: UIButton!) {
+        
+        if tappedTimes < numberOfTapsToWin - 1 {
+            ++tappedTimes
+            tapLabel.text = String(tappedTimes)
+        }
+        else {
+        
+        logo.hidden = false
+        playButton.hidden = false
+        textField.hidden = false
+        tapButton.hidden = true
+        tapLabel.hidden = true
+        
+        textField.text = ""
+        
+        tappedTimes = 0
+        numberOfTapsToWin = 0
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    func alert() {
+        let theAlert = UIAlertController(title: "ALERT", message: "Please add a correct number!", preferredStyle: UIAlertControllerStyle.Alert)
+        let dismissAlert = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        theAlert.addAction(dismissAlert)
+        presentViewController(theAlert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
